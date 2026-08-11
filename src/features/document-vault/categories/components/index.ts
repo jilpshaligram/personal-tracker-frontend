4ba87665d23ef66ad463bcba07a137795192caab
VivekShaligram09 <vivek.j@shaligram.io>
2026-08-11 19:37:50 +0530

    fix: delete doc-vault

--- src/features/document-vault/categories/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { DocumentCategoryList } from './DocumentCategoryList';
--- src/features/document-vault/documents/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { DocumentCard } from './DocumentCard';
-export { DocumentTable } from './DocumentTable';
-export { DocumentFilters } from './DocumentFilters';
-export { DocumentUpload } from './DocumentUpload';
-export { DocumentDetails } from './DocumentDetails';
